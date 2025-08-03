2552e67257dc3e3853e411080f969581e8767618
ChapalBarua <chapalbuet@yahoo.com>
2025-08-03 17:40:18 -0500

    capacitor added

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.chapalbarua',
+  appName: 'International Bridge',
+  webDir: 'dist/internationalBridge/browser'
+};
+
+export default config;
