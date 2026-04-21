e380279925ed55999fb3b770475e1c16f669013a
chapal <chapalbuet@yahoo.com>
2026-04-21 14:20:45 -0500

    including ios build files for first build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bridge.app',
+  appName: 'international-bridge',
+  webDir: 'dist/internationalBridge/browser'
+};
+
+export default config;
