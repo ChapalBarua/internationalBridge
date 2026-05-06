912bcf008fcf6d91745c4cc6a873520086f1119b
chapal <chapalbuet@yahoo.com>
2026-05-06 16:52:56 -0500

    Browser cache refresh when new version is available

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private swUpdate: SwUpdate) {
+    if(!this.swUpdate.isEnabled){
+      return;
+    }
+
+    this.swUpdate.versionUpdates.subscribe((event) => {
+      if(event.type === 'VERSION_READY'){
+        window.location.reload();
+      }
+    });
+  }
+}
